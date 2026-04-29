2ac35788bdd8c2ec6ae52e34e8fd3dd8902b1de0
Jules Sivignon <julessivignon@MacBook-Air-de-Jules.local>
2026-04-29 10:30:35 +0200

    css header

--- application/vues/header.php
+++ application/vues/header.php
@@ -7,13 +7,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/global.css">
+    <link rel="stylesheet" href="../../public/css/header.css">
     <title>Site de concours photo</title>
 </head>
 
 <body>
     <header>
+        <h1>Concours de photographie</h1>
           <!--A modifier plus tard -->
-        <div>
+        <div id="menu">
             <a href="accueil.php">Accueil</a>
             <a href="connexion.php">Se connecter</a>
             <a href="inscription.php">S'inscrire</a>
