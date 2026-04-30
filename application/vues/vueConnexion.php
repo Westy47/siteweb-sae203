e8139ab87f7cced031f9e721992df215f15609ff
Jules Sivignon <julessivignon@MacBook-Air-de-Jules.local>
2026-04-30 10:54:01 +0200

    feat(front): ajouter CSS et restructurer la page de connexion

--- application/vues/vueConnexion.php
+++ application/vues/vueConnexion.php
@@ -1,22 +1,23 @@
 <?php
 require('header.php');
 ?>
-
+<link rel="stylesheet" href="../../public/css/connexion.css">
 <!-- Création du formulaire -->
-<form action="../controleurs/connexion.php" method="post">
-    <div>
-        <h1>Se connecter</h1>
-        <label for="login">Nom utilisateur</label>
-        <input type="text" name="login" id="login">
-        <label for="pwd">Mot de passe</label>
-        <input type="password" name="pwd" id="pwd">
-    </div>
+<div class="main-content">
+    <form action="../controleurs/connexion.php" method="post">
+        <div>
+            <h1>Se connecter</h1>
+            <label for="login">Nom utilisateur</label>
+            <input type="text" name="login" id="login">
+            <label for="pwd">Mot de passe</label>
+            <input type="password" name="pwd" id="pwd">
+        </div>
     <button type="submit">Connexion</button>
-</form>
-
+    </form>
+</div>
 <!-- Message d'erreur si le mdp est incorrect -->
 <?php if(isset($_SESSION['error'])): ?>
-<p><?=$_SESSION['error']?></p>
+<p class="error"><?=$_SESSION['error']?></p>
 <?php unset($_SESSION['error']);
 endif ?>
 
